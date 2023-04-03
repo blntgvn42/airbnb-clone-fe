185f1b387e985d86e7f27038b7c6400a9f55a757
Bulent Guven <bulent.guven@fonetbt.com>
2023-04-03 13:20:27 +0300

    navbar completed

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <div className="flex flex-row items-center gap-2 cursor-pointer" onClick={() => router.push("/")}>
+      <Image src="/images/logo.png" width={100} height={100} alt="Logo" className="hidden md:block cursor-pointer" />
+    </div>
+  );
+};
+
+export default Logo;
